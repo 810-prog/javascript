8bf07e8ae518b7947751b8dd7bc18cba2a2a1b34
dheeraj kumar sain <deerajsain792@gmail.com>
2023-11-01 19:03:17 +0530

    add file

---
+++ index.js
@@ -0,0 +1,101 @@
+
+// var a=29
+// function f(){
+//     console.log(a);
+// }
+// f();
+// console.log(a)
+
+
+// let a= 10;
+// if(true){
+//     let a = 9
+//     console.log(a);
+// }
+// console.log(a);
+
+
+// var a = "string"
+//  a= "dheeraj"
+// //  a = "panakj"
+//  console.log(a)
+
+// let a = 3;
+// let x = (100 + 50) * a;
+// console.log(x);
+
+
+// let text1 = "20";
+// let text2 = "5";
+// let result = text1 > text2;
+// // document.getElementById("demo").innerHTML = "Is 20 less than 5? " + result;
+// console.log(result);
+
+
+// let text1 = "What a very ";
+// text1 += "nice day ";
+// text1 += "today im good ";
+// text1+= "and what about you ";
+// console.log(text1);
+
+
+// let x = 5 + 5;
+// let y = "5" + 5;
+// let z = "Hello " + 5+7;
+// console.log(x, y, z);
+
+// let i=1;
+// while(i<=5)
+// {
+//     console.log("vijay");
+//     i++
+// }
+
+
+// //ternory operater
+// let age = 28;
+// age>28?console.log("you can vote") : console.log("you cann't vote");
+
+
+
+//string operater 
+
+// a ='dheeraj '
+// b='sain'
+// console.log(a+b)
+
+
+// a= "bhasker"
+// b= "yogi"
+// myname= a+b;
+// console.log("i am " + myname)
+
+// a= "bhasker"
+// b= "yogi"
+// myname= a+b;
+// console.log(`i am  $ {myname}`)
+
+
+// a="jai";
+// b="vijay";
+// console.log(`${a} * ${b}`);//templet litral
+
+
+//data type   
+// 1 b  - BigInt
+// 2 b-  Boolean
+// 3s - Symbol
+// 4 s- String
+// 5 n - Number
+// 6 u undefined
+// 7 n - null
+
+// console.log(typeof(a));
+
+// var a = undefined
+// console.log(a);
+   
+
+console.log(typeof(a));
+
+
